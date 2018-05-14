935a8fc40b6ea79bb2f5f7fb3080955a44e9f079
meldan3n <geforce777@web.de>
2018-05-14 18:58:59 +0200

    2.8 task changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  root 'simple_pages#index'
 end
